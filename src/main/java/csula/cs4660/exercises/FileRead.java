583bc677f520277a7a9b91b544496fdb9f125ece
Antonio Bang <anto_004@ymail.com>
2016-08-29 14:39:47 -0700

    adding file read

--- src/main/java/csula/cs4660/exercises/FileRead.java
+++ src/main/java/csula/cs4660/exercises/FileRead.java
@@ -1,6 +1,11 @@
 package csula.cs4660.exercises;
 
 import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.FileInputStream;
+import java.io.InputStream;
 
 /**
  * Introduction Java exercise to read file
@@ -13,7 +18,33 @@ public class FileRead {
      */
     public FileRead(File file) {
         // TODO: read the file content and store content into numbers
+        InputStream is = null;
+        int i;
+        String c;
+        String[] str = null;
+        int j=0;
+
+        try{
+            // new input stream created
+            is = new FileInputStream(file);
+
+            System.out.println("Characters printed:");
+
+            // reads till the end of the stream
+            while((i=is.read())!=-1)
+            {
+                str[j++]= String.valueOf(i);
+
+            }
+            System.out.println(str);
+        }catch(Exception e){
+
+            // if any I/O error occurs
+            e.printStackTrace();
+        }
+
     }
+
 
     /**
      * Read the file assuming following by the format of split by space and next
@@ -37,4 +68,10 @@ public class FileRead {
     public int sum(int lineNumber) {
         return 0;
     }
+
+    public static void main(String [] args) {
+        File file = new File("/Users/anto004/Desktop/AI/cs4660-fall-2016/src/main/resources/array.txt");
+        FileRead fr = new FileRead(file);
+    }
+
 }
