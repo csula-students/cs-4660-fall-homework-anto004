247133602cb42752437492d594e2cdadce5e947e
Antonio Bang <anto_004@ymail.com>
2016-09-11 13:32:15 -0700

    getting edges from the graph

--- src/main/java/csula/cs4660/graphs/representations/AdjacencyList.java
+++ src/main/java/csula/cs4660/graphs/representations/AdjacencyList.java
@@ -1,13 +1,14 @@
 package csula.cs4660.graphs.representations;
 
+import com.google.common.collect.Lists;
 import csula.cs4660.graphs.Edge;
 import csula.cs4660.graphs.Node;
 
 import java.io.File;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.*;
+import java.util.stream.Stream;
 
 /**
  * Adjacency list is probably the most common implementation to store the unknown
@@ -19,6 +20,47 @@ public class AdjacencyList implements Representation {
     private Map<Node, Collection<Edge>> adjacencyList;
 
     public AdjacencyList(File file) {
+        List<Integer> graph = Lists.newArrayList();
+        List<Edge> edges = Lists.newArrayList();
+        HashMap nodes = new HashMap();
+        try (Stream<String> stream = Files.lines(file.toPath())) {
+            stream.forEach(line -> {
+                for (String token: line.split(" ")) {
+                    if(token.contains(":")) {
+                        String[] currentLine = token.split(":");
+                        nodes.put("node" + token, Integer.parseInt(currentLine[0]));
+
+                        Node fromNode = new Node(Integer.parseInt(currentLine[0]));
+                        Node toNodeEdge = new Node(Integer.parseInt(currentLine[1]));
+                        Integer edgeValue = Integer.parseInt(currentLine[2]);
+
+                        Edge edge = new Edge(fromNode, toNodeEdge, edgeValue);
+                        edges.add(edge);
+                    }
+
+                    //graph.add(Integer.parseInt(token));
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for(Edge edge: edges){
+            System.out.println(edge);
+        }
+
+        System.out.println("/n /n HashMap values");
+        // Get a set of the entries
+        Set set = nodes.entrySet();
+        // Get an iterator
+        Iterator i = set.iterator();
+        // Display elements
+        while(i.hasNext()) {
+            Map.Entry me = (Map.Entry)i.next();
+            System.out.print(me.getKey() + "--");
+            System.out.println(me.getValue());
+        }
+
     }
 
     public AdjacencyList() {
---
+++ src/main/java/csula/cs4660/graphs/representations/MyTest.java
@@ -0,0 +1,16 @@
+package csula.cs4660.graphs.representations;
+
+import java.io.File;
+
+/**
+ * Created by anto004 on 9/11/16.
+ */
+public class MyTest {
+    public static void main(String [] args){
+
+        File file1 = new File("/Users/anto004/Desktop/AI/cs4660-fall-2016/src/test/resources/homework-1/graph-1.txt");
+
+        AdjacencyList al = new AdjacencyList(file1);
+
+    }
+}
