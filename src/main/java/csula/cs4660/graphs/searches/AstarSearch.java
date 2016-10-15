e26c06a8610cbb86e54274b60d4dde5a3c18646d
Antonio Bang <anto_004@ymail.com>
2016-10-15 14:57:23 -0700

    Astar queue

--- src/main/java/csula/cs4660/graphs/searches/AstarSearch.java
+++ src/main/java/csula/cs4660/graphs/searches/AstarSearch.java
@@ -1,6 +1,7 @@
 package csula.cs4660.graphs.searches;
 
 import com.google.common.collect.Lists;
+import csula.cs4660.games.models.Tile;
 import csula.cs4660.graphs.Edge;
 import csula.cs4660.graphs.Graph;
 import csula.cs4660.graphs.Node;
@@ -15,64 +16,90 @@ public class AstarSearch implements SearchStrategy {
     @Override
     public List<Edge> search(Graph graph, Node source, Node dist) {
         System.out.println("AStar");
-        Queue<Node> frontier = new PriorityQueue<Node>(11, new MyComparator());
         Map<Node, Node> exploredMap = new HashMap<>();
         Map<Node, Node> parents = new HashMap<>();
-
-        source.setDistance(0);
-        frontier.add(source);
 
         Map<Node, Integer> gscore = new HashMap<>();
         gscore.put(source, 0);
 
         Map<Node, Integer> fscore = new HashMap<>();
-        //fscore.put(source, heuristicCost(source, dist));
+        fscore.put(source, heuristicCost(source, dist));
 
-        while(!frontier.isEmpty()){
+        Queue<Node> frontier = new PriorityQueue<Node>((node1, node2)->{
+            int n1Score = fscore.get(node1), n2Score = fscore.get(node2);
+
+            if (n1Score < n2Score) {
+                return -1;
+            } else if (n1Score == n2Score) {
+                int g1score = gscore.get(node1);
+                int g2score = gscore.get(node2);
+
+                if (g1score < g2score)
+                    return 0;
+                else if (g1score > g2score)
+                    return 7;
+                else {
+                    if (isNorth(node1, node2))
+                        return 2;
+                    else if (isEast(node1, node2))
+                        return 3;
+                    else if (isWest(node1, node2))
+                        return 4;
+                    else if (isSouth(node1, node2))
+                        return 5;
+                    else
+                        return 6;
+                }
+            } else return 7;
+        });
+
+        source.setDistance(0);
+        frontier.add(source);
+
+        while (!frontier.isEmpty()) {
             // pop with the lowest fscore
             Node u = frontier.poll();
-            System.out.println("frontier.poll():"+u);
-            if(u.equals(dist)) {
+            System.out.println("frontier.poll():" + u);
+            if (u.equals(dist)) {
                 System.out.println("found goal");
 
-                for(Node node: exploredMap.keySet()){
-                    System.out.println("Explored Node-"+exploredMap.get(node));
+                for (Node node : exploredMap.keySet()) {
+                    System.out.println("Explored Node-" + exploredMap.get(node));
                 }
-                for(Node node: parents.keySet()){
-                    System.out.println("Parent of "+ node + "is:"+ parents.get(node));
+                for (Node node : parents.keySet()) {
+                    System.out.println("Parent of " + node + "is:" + parents.get(node));
                 }
 
                 return constructPath(graph, u, parents);
             }
-            exploredMap.put(u,u);
+            exploredMap.put(u, u);
 
-            for(Node node: graph.neighbors(u)){
-                System.out.println("Neighbor of "+ u+" is " + node);
-                if(exploredMap.containsValue(node)){
-                    System.out.println("Explored map already contains"+exploredMap.get(node));
+            for (Node node : graph.neighbors(u)) {
+                System.out.println("Neighbor of " + u + " is " + node);
+                if (exploredMap.containsValue(node)) {
+                    System.out.println("Explored map already contains" + exploredMap.get(node));
                     continue;
                 }
 //                else
 //                    node = exploredMap.get(node);
-                if(gscore.get(node) == null)
+                if (gscore.get(node) == null)
                     node.setDistance(Integer.MAX_VALUE);
-                else{
+                else {
                     node.setDistance(gscore.get(node));
-                    System.out.println(node+"Setting Distance to "+ gscore.get(node));
+                    System.out.println(node + "Setting Distance to " + gscore.get(node));
                 }
                 int tempGScore = gscore.get(u) + graph.distance(u, node);
-                System.out.println("tempGScore="+tempGScore);
+                System.out.println("tempGScore=" + tempGScore);
 
-                if(tempGScore < node.getDistance()){
+                if (tempGScore < node.getDistance()) {
                     node.setDistance(tempGScore);
                     parents.put(node, u);
-                    System.out.println(node+"is being set distance"+node.getDistance());
+                    System.out.println(node + "is being set distance" + node.getDistance());
                 }
 
-                if(!frontier.contains(node)){
+                if (!frontier.contains(node)) {
                     frontier.add(node);
-                }
-                else if(tempGScore >= gscore.get(node)){
+                } else if (tempGScore >= gscore.get(node)) {
                     continue;
                 }
                 gscore.put(node, tempGScore);
@@ -83,37 +110,55 @@ public class AstarSearch implements SearchStrategy {
         return null;
     }
 
-    public List<Edge> constructPath(Graph graph, Node node, Map<Node,Node> parents){
+    public List<Edge> constructPath(Graph graph, Node node, Map<Node, Node> parents) {
         Node endTile = new Node(node.getData());
         List<Edge> result = Lists.newArrayList();
         List<Edge> reverseList = Lists.newArrayList();
 
-        while(parents.get(endTile) != null){
+        while (parents.get(endTile) != null) {
             Node fromNode = new Node(parents.get(endTile).getData());
             Node toNode = new Node(endTile.getData());
             result.add(new Edge(fromNode, toNode, graph.distance(fromNode, toNode)));
             endTile = new Node(parents.get(endTile).getData());
         }
-        for(ListIterator<Edge> iterator = result.listIterator(result.size()); iterator.hasPrevious();){
+        for (ListIterator<Edge> iterator = result.listIterator(result.size()); iterator.hasPrevious(); ) {
             reverseList.add(iterator.previous());
         }
 
-        for(Edge edge: result){
-            System.out.println("A star Result list:"+ edge);
+        for (Edge edge : result) {
+            System.out.println("A star Result list:" + edge);
         }
-        for(Edge edge: reverseList){
-            System.out.println("Reversed list"+edge);
+        for (Edge edge : reverseList) {
+            System.out.println("Reversed list" + edge);
         }
         return reverseList;
     }
-}
-class MyComparator implements Comparator<Node>{
 
-    @Override
-    public int compare(Node node1, Node node2) {
-        if(node1.getDistance() < node2.getDistance())
-            return -1;
-        else
-            return 1;
+
+    private int heuristicCost(Node node, Node goal) {
+        Tile nodeTile = (Tile) node.getData();
+        Tile goalTile = (Tile) goal.getData();
+
+        int dx = Math.abs(nodeTile.getX() - goalTile.getX());
+        int dy = Math.abs(nodeTile.getY() - goalTile.getY());
+
+        return dx + dy;
+    }
+
+    private boolean isNorth(Node src, Node goal) {
+        return ((Tile) src.getData()).getY() > ((Tile) goal.getData()).getY();
+    }
+
+    private boolean isSouth(Node src, Node goal) {
+        return ((Tile) src.getData()).getY() < ((Tile) goal.getData()).getY();
+    }
+
+    private boolean isWest(Node src, Node goal) {
+        return ((Tile) src.getData()).getX() > ((Tile) goal.getData()).getX();
+    }
+
+    private boolean isEast(Node src, Node goal) {
+        return ((Tile) src.getData()).getX() < ((Tile) goal.getData()).getX();
     }
 }
+
