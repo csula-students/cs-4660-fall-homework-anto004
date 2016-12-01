faddf7e784b14e11ced7bd308ed414f5717877b7
Antonio Bang <anto_004@ymail.com>
2016-11-30 18:06:47 -0800

    working on bfs

--- src/main/java/csula/cs4660/project/Tron.js
+++ src/main/java/csula/cs4660/project/Tron.js
@@ -6,16 +6,9 @@ var board = [];
 for (var i = 0; i < 30; i++) { //30cols: represents x axis
     board[i] = new Array(20);
     for (var j = 0; j < 20; j++) //20rows: represents y axis
-        board[i][j] = {tile: {x: i, y: j}, value: 0};
+        board[i][j] = {x: i, y: j, value: 0};
 }
 
-//debug variables
-var debugBoard = [];
-for(var i=0; i<30; i++){
-    debugBoard[i] =  new Array(20);
-    for(var j=0; j<20; j++)
-        debugBoard[i][j] = 0;
-}
 
 function getCell(board, y, x) {
     var NO_VALUE = null;
@@ -40,41 +33,51 @@ function getNeighbors(board, y, x) {
     };
 }
 
-
-var leftWall = 0;
-var rightWall = 29;
-var topWall = 0;
-var bottomWall =19
-
-// game loop
-while (true) {
-    var inputs = readline().split(' ');
-    var N = parseInt(inputs[0]); // total number of players (2 to 4).
-    var P = parseInt(inputs[1]); // your player number (0 to 3).
-    for (var i = 0; i < N; i++) {
-        var inputs = readline().split(' ');
-        var X0 = parseInt(inputs[0]); // starting X coordinate of lightcycle (or -1)
-
-        var Y0 = parseInt(inputs[1]); // starting Y coordinate of lightcycle (or -1)
-
-        var X1 = parseInt(inputs[2]); // starting X coordinate of lightcycle (can be the same as X0 if you play before this player)
-
-        var Y1 = parseInt(inputs[3]); // starting Y coordinate of lightcycle (can be the same as Y0 if you play before this player)
-
-        //printErr('X0 '+ X0 + 'Y0 '+ Y0);
-        printErr('X1 '+ X1 + 'Y1 '+ Y1);
-        debugBoardFunc(X0,Y0,X1,Y1);
-
-        // if(i === P)
-        //     board[Y0][X0] = {x:X0, y:Y0};
-        //     board[Y1][X1] = {x:X1, y:Y1};
-        //     minimax(X1, Y1, 1, true);
-    move(X1,Y1);
+function getNoOfChildren(neighbors){
+    var num = 0;
+    for(var neighbor in neighbors){
+        if(neighbors[neighbor] !== null)
+            num += 1;
     }
+    return num;
+}
 
-    // Write an action using print()
-    // To debug: printErr('Debug messages...');
+function BFS(X0,Y0){ //for now lets just store values for p1
+    //queue
+    var queue = [];
+    var source = board[Y0][X0] = {x:X0, y:Y0, value:0};
 
+    queue.push(source);
+    var exploredSet = new Set();
+    exploredSet.add(source);
+
+    while(queue.length !== 0){
+        var count =0;
+        console.log('level'+ count ++);
+        var u = queue.shift();
+        var x = u.x;
+        var y = u.y;
+
+        var neighbors = getNeighbors(board, y, x);
+        var noOfChild = getNoOfChildren(neighbors);
+        for(var neighbor in neighbors){ // neighbor -> up,right,down,left
+//            console.log(neighbors[neighbor]);
+//            if(test === neighbors[neighbor]){
+//                console.log('same type safe to add');
+//            }
+            if(neighbors[neighbor] !== null){
+                x = neighbors[neighbor].x;
+                y = neighbors[neighbor].y;
+
+                //x and y as acc to the board initialization set the value
+                board[x][y].value = 1 / noOfChild;
+                if(!exploredSet.has(neighbors[neighbor])){
+                    queue.push(neighbors[neighbor]);
+                    exploredSet.add(neighbors[neighbor]);
+                }
+            }
+        }
+    }
 }
 
 function minimax(X1, Y1, depth, max ){
@@ -86,28 +89,52 @@ function minimax(X1, Y1, depth, max ){
     // }
     printErr('board of y1 and x1 '+board[Y1][X1].x +' '+board[Y1][X1].y)
 }
+BFS(10,10);
+//debugBoard();
 
-function move(X1,Y1){
-        if(X1 === leftWall || X1 === rightWall)
-            print('UP');
-        else if(Y1 === topWall || Y1 === bottomWall)
-            print('RIGHT')
-        else
-            print('LEFT');// A single line with UP, DOWN, LEFT or RIGHT
-}
-
-
-function debugBoardFunc(X0,Y0,X1,Y1){
-    board[X0][Y0] = {x:1, y:1};
-    board[X1][Y1] = {x:1, y:1};
+function debugBoard(){
+    //board[X0][Y0] = {x:1, y:1};
+    //board[X1][Y1] = {x:1, y:1};
 
         // for(var i=0; i<30; i++){
         //     board[i].forEach(function(is){ is.forEach(function(his){ printErr(his.x)})});
         // }
     for(var i = 0; i < board.length; i++) {
-    var cube = board[i];
-    for(var j = 0; j < cube.length; j++) {
-        printErr("board[" + i + "][" + j + "] = " + cube[j].x + ' '+cube[j].y);
+        var cube = board[i];
+        for(var j = 0; j < cube.length; j++) {
+            console.log("board[" + i + "][" + j + "] = " + board[i][j].value);
+        }
     }
 }
-}
+
+//// game loop
+//while (true) {
+//    var inputs = readline().split(' ');
+//    var N = parseInt(inputs[0]); // total number of players (2 to 4).
+//    var P = parseInt(inputs[1]); // your player number (0 to 3).
+//    for (var i = 0; i < N; i++) {
+//        var inputs = readline().split(' ');
+//        var X0 = parseInt(inputs[0]); // starting X coordinate of lightcycle (or -1)
+//
+//        var Y0 = parseInt(inputs[1]); // starting Y coordinate of lightcycle (or -1)
+//
+//        var X1 = parseInt(inputs[2]); // starting X coordinate of lightcycle (can be the same as X0 if you play before this player)
+//
+//        var Y1 = parseInt(inputs[3]); // starting Y coordinate of lightcycle (can be the same as Y0 if you play before this player)
+//
+//        //printErr('X0 '+ X0 + 'Y0 '+ Y0);
+//        printErr('X1 '+ X1 + 'Y1 '+ Y1);
+//        debugBoardFunc(X0,Y0,X1,Y1);
+//
+//        // if(i === P)
+//        //     board[Y0][X0] = {x:X0, y:Y0};
+//        //     board[Y1][X1] = {x:X1, y:Y1};
+//        //     minimax(X1, Y1, 1, true);
+//    move(X1,Y1);
+//    }
+//
+//    // Write an action using print()
+//    // To debug: printErr('Debug messages...');
+//
+//}
+
